d22d0267946711c1fb3188c17fa6c18b544c2742
CameronPinchin <cameronpinchin@cmail.carleton.ca>
2026-04-30 18:59:26 -0400

    typo fix, buffer size

--- src/pitemp.c
+++ src/pitemp.c
@@ -4,7 +4,7 @@
 #include <linux/cdev.h>
 #include <linux/fs.h>
 #include <linux/types.h>
-#include <asm/uaccess.h
+#include <asm/uaccess.h>
 
 #define DRIVER_AUTHOR           "Cameron Pinchin <cwpinchin@outlook.com>"
 #define DRIVER_DESC             "Driver for temperature sensor on the RP1 I/O Controller"
@@ -14,7 +14,7 @@
 
 struct temp_device_data {
     struct cdev cdev;
-    char buffer[];          // unsure what to size
+    char buffer[1024];          // unsure what to size
     int16_t size;           // likely change, provides 16-bits though
 }
 
